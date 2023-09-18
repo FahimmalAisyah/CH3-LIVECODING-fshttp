0cb8a1c315993db64c96acb3f5fb6c82dd55a40c
FahimmalAisyah <fahimmal.sasad@gmail.com>
2023-09-18 22:49:07 +0700

    dynamic data

--- index.js
+++ index.js
@@ -1,105 +1,100 @@
-const fs = require("fs");
-const http = require("http");
-const url = require("url");
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
-//////////////////////
-// FILE FS
+///////////////////////////////////
+// FILES dengan FS
 
 // blocking code execution => synchronous
 // const textIn = fs.readFileSync('./txt/read-this.txt', 'utf-8');
 // console.log(textIn);
 
-// const textOut = 'ini tuh penjelasan tentang alpukat di bahasa inggris : ${textIn}';
+// const textOut = `ini tuh penjelasan ttg alpukat di bahasa inggris : ${textIn}`;
 // fs.writeFileSync('./txt/output-penjelasan.txt', textOut);
 // console.log('sukses nyetak data avocado!');
 
-// non-blocking code execution => asynchronous
-// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-//   fs.readFile("./txt/${data}.txt", "utf-8", (err, data2) => {
-//     fs.readFile("./txt/final.txt", "utf-8", (err, data3) => {
+// non-blocking code execution => asynchronous 
+// fs.readFile('./txt/startafjoafhuawfuaufhawuf.txt', 'utf-8', (err, data) => {    
+//     if (err) return console.log('ini ada error di read file 1');
+//     fs.readFile(`./txt/${data}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
 
-//     fs.writeFile("./txt/gabungan.txt", "${data}\n$(data2)", (err) => {
-//       console.log("sukses menggabungkan data");
-
-//     fs.writeFile('./txt/gabungan-read-this.txt', "${data}\n$(data3)", (err) => {
-//         console.log("gabungan data")
-
+//         fs.writeFile(`./txt/gabungan.txt`, `${data}\n${data2}`, err => {
+//             console.log('sukses menggabungkan data');
+//         })
 //     });
-//      });
-//     });
-//   });
 // });
-// console.log("hai FSW 2 menunggu");
+// console.log('hai FSW 2 nunggu read file yah?');
 
-//////////////////
-// SERVER DENGAN HTTP
+/////////////////////////////////////////
+// SERVER dengan HTTP
 const replaceTemplate = (template, product) => {
-  console.log(product)
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%ID%}/g, product.id);
+    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%ID%}/g, product.id);
 
-  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, `not-organic`);
-  return output;
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
 }
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, `utf-8`);
-const dataOBj = JSON.parse(data);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
 
-const overviewPage = fs.readFileSync(`${__dirname}/templates/overview.html`, `utf-8`);
-const productTemplate = fs.readFileSync(`${__dirname}/templates/product.html`, `utf-8`);
-const productCardTemplate = fs.readFileSync(`${__dirname}/templates/template-card.html`, `utf-8`);
+const overviewPage = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
+const productTemplate = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
+const productCardTemplate = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 
 const server = http.createServer((req, res) => {
-  // let = url.parse(req.url, true). pathname;
-  const {query} = url.parse(req.url, true).query;
-  const pathName = req.url;
-  console.log(pathName)
+    const { pathname: pathName, query } = url.parse(req.url, true);
+    console.log(pathName)
+    console.log(query)
 
-  // HELLO PAGE
-  if (pathName === "/hello") {
-    res.end("ini hello ke FSW 2");
+    // HELLO PAGE 
+    if (pathName === '/hello') {
+        res.end('ini hello ke FSW 2')
 
-  // PRODUCT PAGE
-  } else if (pathName === "/product") {
-    res.end(JSON.stringify({
-        data: "ini product",
-      }));
+        // simple api
+    } else if (pathName === '/api') {
+        res.writeHead(200, {
+            'Content-type': 'application/json'
+        });
+        res.end(data);
 
-  // SIMPLE API
-    } else if (pathName === '/api'){
-      res.writeHead(200, {
-        "content-type" : "application/json",
-    });
-    res.end(data);
-
-  // OVERVIEW PAGE 
+        // overview page 
     } else if (pathName === '/overview') {
-      res.writeHead(200, {
-        "content-type" : `text.html`,
-      });
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        })
 
-      const productCardHtml = dataOBj.map(el => replaceTemplate(productCardTemplate, el));
-      const output = overviewPage.replace(`%PRODUCT_CARDS%`, productCardHtml)
-      res.end(output);
+        const productCardsHtml = dataObj.map(el => replaceTemplate(productCardTemplate, el));
+        const output = overviewPage.replace('{%PRODUCT_CARDS%', productCardsHtml);
 
-      
+        res.end(output);
 
-  // TIDAK ADA APA APA
-  } else {
-    res.writeHead(404, {  
-      "content-type" : "text/html",
-    });
-    res.end("<h1>url ini gak ada apa2</h1>");
-  }
 
+        //  product page
+    } else if (pathName === '/product') {
+        console.log(query)
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        })
+        const product = dataObj[query.id];
+        const output = replaceTemplate(productTemplate, product);
+        res.end(output);
+        
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html'
+        })
+        res.end('<h1>url ini gak ada apa2 bestie!!!</h1>');
+    }
 });
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Hello Server nya Jalan!!!");
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Hello Server nya Jalann!!!');
 });
